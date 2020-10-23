ccb9a5c33c62b7a3c9e9358ea0ac4e87fcc3623e
abuhani <>
2020-10-23 21:49:11 +0300

    Core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { SpoonacularHttpClient } from './intercepter/spoonacular.intercepter';
+
+const components = [
+  MainLayoutComponent
+];
+
+@NgModule({
+  declarations: [...components],
+  exports: [...components],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SpoonacularHttpClient,
+      multi: true
+    },
+  ]
+})
+export class CoreModule { }
