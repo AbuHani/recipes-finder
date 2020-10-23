be34ceed2d79284294d7960aff456e4db8f8c7c5
abuhani <>
2020-10-23 21:48:20 +0300

    Initialize app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'recipes'
+  },
+  {
+    path: 'recipes',
+    loadChildren: () => import('./features/recipes/recipes.module').then(m => m.RecipesModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'recipes'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
