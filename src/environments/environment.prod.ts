4ee7194abd8ccc853894073f6cfbc8a2f674b563
abuhani <>
2020-10-25 02:42:00 +0200

    Add more keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,11 @@ export const environment = {
   spoonacular: {
     baseUrl: 'https://api.spoonacular.com/',
     keys: [
-      '36a4609876c146509be11f98b097011d',
       '84d547a31b13472887505c20cd5a4245',
-      '36a4609876c146509be11f98b097011d'
+      '55d1ef367376428c82c005f334b035ce',
+      '36a4609876c146509be11f98b097011d',
+      'f9611665da104479856c1fac0c6032c9',
+      '66902da9ef0542c28124109ceabddb71'
     ]
   }
 };
